3f61ac6e949625258abb9b6ae0334d5e97c248a1
Henning Wuehn <hwuehn@local>
2019-09-27 08:51:34 +0200

    feature: added new object in main() for testing testFilePath()

--- src/de.company.filepathhelper/application/Main.java
+++ src/de.company.filepathhelper/application/Main.java
@@ -1,8 +1,13 @@
 package application;
 
+import data.PathManager;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        PathManager test = new PathManager();
+        test.testFilePath();
 
 
     }
--- src/de.company.filepathhelper/data/PathManager.java
+++ src/de.company.filepathhelper/data/PathManager.java
@@ -9,7 +9,7 @@ public class PathManager {
     public static void testFilePath() {
         Scanner s = null;
         try {
-            File file = new File("src\\de.company.filepathelper\\data\\PathManager.java");
+            File file = new File("src\\de.company.filepathhelper\\data\\PathManager.java");
             s = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
