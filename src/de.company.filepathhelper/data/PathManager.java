8a6286734610d2920761fbf956bf67abc9c27dfd
Henning Wuehn <hwuehn@local>
2019-09-27 08:48:01 +0200

    feature: added PathManager class with testFileMethod

---
+++ src/de.company.filepathhelper/data/PathManager.java
@@ -0,0 +1,26 @@
+package data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PathManager {
+
+    public static void testFilePath() {
+        Scanner s = null;
+        try {
+            File file = new File("src\\de.company.filepathelper\\data\\PathManager.java");
+            s = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (s.hasNext()) {
+            String string = s.nextLine();
+            if (string != null) {
+                System.out.println(string.lines().count());
+            }
+        }
+
+
+    }
+}
